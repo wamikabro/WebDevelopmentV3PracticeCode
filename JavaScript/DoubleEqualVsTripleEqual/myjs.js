7754a2b62e8aeb2bb0b469dbe2a7dd1ecf2e334d
Wamique Abro <wamik.abro212@gmail.com>
2024-04-22 04:49:43 +0500

    double equal vs triple equal

---
+++ JavaScript/DoubleEqualVsTripleEqual/myjs.js
@@ -0,0 +1,6 @@
+let number = 1;
+
+if(number == "1")
+    console.log("Double Equal");  // this one will execute only
+if(number === "1") // it won't because it strictly checks datatype too
+    console.log("Triple Equal"); 
