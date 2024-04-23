04dbedafd0995ef03fc27be9c31764214d9b9406
Wamique Abro <wamik.abro212@gmail.com>
2024-04-23 20:04:23 +0500

    padStart

---
+++ JavaScript/PadStart/myjs.js
@@ -0,0 +1,4 @@
+const timesToRepeat = 10;
+const character = '💜';
+console.log("".padStart(timesToRepeat,character)); // it will repeat 10 times, and add purple heart each time
+// to the "" empty string.
