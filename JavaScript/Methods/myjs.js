2b40c0d8272a0ecdbafa0f6e9f0fa3051ff5cc71
Wamique Abro <wamik.abro212@gmail.com>
2024-04-26 04:25:55 +0500

    methods in objects

---
+++ JavaScript/Methods/myjs.js
@@ -0,0 +1,14 @@
+const cat = {
+    name: "muffen",
+    meow(){
+        console.log("meow");
+    },
+
+    longMeow: function(){
+        console.log("meowwww");
+    }
+};
+
+
+cat.meow()
+cat.longMeow()
