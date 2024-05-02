dccb74ae5a23fd4d8d09c325dc4c82de91a7b02a
Wamique Abro <wamik.abro212@gmail.com>
2024-05-02 20:12:48 +0500

    synchronous callback

---
+++ JavaScript/SyncAsyncCallbackPromise/SynchroniousCallback/myjs.js
@@ -0,0 +1,14 @@
+function getData(dataId, getNextData){
+    setTimeout(
+        () => {
+        console.log("data", dataId)
+        if(getNextData){
+            getNextData();
+        }
+    }
+    , 2000)
+
+    getData(1, () => {
+        getData(2);
+    })
+}
