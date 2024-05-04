c71e77b7853a2fce8bb59c5c748b3dc8c8f81992
Wamique Abro <wamik.abro212@gmail.com>
2024-05-05 03:00:59 +0500

    iife

---
+++ JavaScript/Iife/myjs.js
@@ -0,0 +1,66 @@
+const getPromise = (number) => {
+   return new Promise((resolve, reject) => {
+       setTimeout(() => {
+           if(number < 5)
+               resolve("selected.");
+           else {
+               reject("rejected.");
+           }    
+       }, 2000);
+   });
+}
+
+// to make it one time called function we will 
+// make it iffe. example below this function.
+async function myFunc(){
+    const function1 = await getPromise(1); 
+    console.log("function 1: ", function1)
+    const function2 = await getPromise(2); 
+    console.log("function 2: ", function2); 
+}   
+myFunc();
+
+
+
+// but the name for the function is unneeded
+// since we won't be able to call it again.
+(async function myFunc1(){
+    const function1 = await getPromise(1); 
+    console.log("function 1: ", function1)
+    const function2 = await getPromise(2); 
+    console.log("function 2: ", function2); 
+})();
+
+
+// now it's alright
+(async function (){
+    const function1 = await getPromise(1); 
+    console.log("function 1: ", function1)
+    const function2 = await getPromise(2); 
+    console.log("function 2: ", function2); 
+})();
+
+// we can call arrow function or simple function too.
+( function (){
+    const function1 = getPromise(1); 
+    console.log("function 1: ", function1)
+    const function2 = getPromise(2); 
+    console.log("function 2: ", function2); 
+})();
+
+(() => {
+    const function1 = getPromise(1); 
+    console.log("function 1: ", function1)
+    const function2 = getPromise(2); 
+    console.log("function 2: ", function2); 
+})();
+
+// async arrow function
+(async () => {
+    const function1 = await getPromise(1); 
+    console.log("function 1: ", function1)
+    const function2 = await getPromise(2); 
+    console.log("function 2: ", function2); 
+})();
+
+
