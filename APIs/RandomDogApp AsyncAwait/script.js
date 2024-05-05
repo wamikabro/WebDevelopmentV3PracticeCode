f5453b9462f500003520386102405eb6fcbf6b41
Wamique Abro <wamik.abro212@gmail.com>
2024-05-05 05:57:23 +0500

    comment to async await

--- APIs/RandomDogApp AsyncAwait/script.js
+++ APIs/RandomDogApp AsyncAwait/script.js
@@ -6,6 +6,10 @@ const container = document.querySelector('.container'); // to get final dog as i
 async function addNewDog(){
     const response = await fetch(url);
     const processingResponse = await response.json();
+
+    // single line was also possible but less readable.
+    // const processingResponse = await (await fetch(url)).json();
+
     const image = document.createElement("img");
     image.src = processingResponse.message;
     container.appendChild(image);
