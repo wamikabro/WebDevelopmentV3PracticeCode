bf8ffa31655b094d518c71ecd89bca08adf25066
Wamique Abro <wamik.abro212@gmail.com>
2024-05-14 17:02:30 +0500

    connect external libraries through parcel build

---
+++ OtherStuff/ThirdPartyLibraries/BallAnimation/animation.js
@@ -0,0 +1,13 @@
+const popmotion = require('popmotion')
+const ball = document.querySelector('.ball');
+
+popmotion.animate({
+    from: '0px',
+    to: '250px',
+    repeat: Infinity,
+    repeatType: 'mirror',
+    type: 'spring',
+    onUpdate(update){
+        ball.style.top = update;
+    }
+})
